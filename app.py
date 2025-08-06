760f31bcd04f81be7dee5e2b4f6e97ba5dd7d7a0
daniel-hy-chen-appier <daniel.hy.chen@appier.com>
2025-08-06 15:19:01 +0800

    connect gpt to slack

--- app.py
+++ app.py
@@ -11,56 +11,169 @@ from dotenv import load_dotenv
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import getopt
+import random
+import time
+import shutil
 load_dotenv()
-# gauth = GoogleAuth()
-# gauth.LoadCredentialsFile("creds.txt")
-# if gauth.credentials is None:
-#     # Authenticate if they're not there
-#     gauth.LocalWebserverAuth()
-# elif gauth.access_token_expired:
-# # Refresh them if expired
-#     gauth.Refresh()
-# else:
-#     # Initialize the saved creds
-#     gauth.Authorize()
-# # Save the current credentials to a file
-# gauth.SaveCredentialsFile("creds.txt")
+retries = 4
 
-# def authorize_drive():
-#     gauth = GoogleAuth()
-#     gauth.DEFAULT_SETTINGS['client_config_file'] = "client_secrets.json"
-#     gauth.LoadCredentialsFile("creds.txt")
-#     return GoogleDrive(gauth)
+def authorize_drive():
+    gauth = GoogleAuth()
+    gauth.DEFAULT_SETTINGS['client_config_file'] = "creds/client_secrets.json"
+
+    gauth.LoadCredentialsFile("creds/creds.txt")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+    # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("creds/creds.txt")
+    
+    return GoogleDrive(gauth)
 
 
-# class Drive_object(object):
-#     def __init__(self):
-#         self.drive = authorize_drive() 
+class Drive_object(object):
+    def __init__(self):
+        self.drive = authorize_drive() 
+    # will need create(new), download(exist), upload(exist)
+    # create also need to return id
 
+    def create_new_file(self, local_path, new_name, folder_id, return_id=False):
+        count = 0
+        file = self.drive.CreateFile({'title':new_name, 'parents':[{'id':folder_id}]})
+        file.SetContentFile(local_path)
+        while True:
+            try:
+                file.Upload()
+                if return_id == True:
+                    return file['id']
+            except:                
+                if count == retries:                    
+                    raise
+                sleep = 2 ** count + random.uniform(0, 1)
+                time.sleep(sleep)
+                count += 1
+
+    def download_file(self, local_path, file_id):
+        count = 0
+        file = self.drive.CreateFile({'id':file_id})
+        while True:
+            try:
+                file.GetContentFile(local_path)
+                return
+            except:
+                if count == retries:                    
+                    raise
+                sleep = 2 ** count + random.uniform(0, 1)
+                time.sleep(sleep)
+                count += 1                
+        
+    
+    def update_file(self, local_path, file_id):
+        count = 0
+        file = self.drive.CreateFile({'id': file_id})
+        file.SetContentFile(local_path)
+        while True:
+            try:                
+                file.Upload()
+                return
+            except:
+                if count == retries:
+                    raise
+                sleep = 2 ** count + random.uniform(0, 1)
+                time.sleep(sleep)
+                count += 1
+    def download_folder(self, folder_id, local_path = '.'):
+        if not os.path.exists(local_path):
+            os.makedirs(local_path)
+
+        file_list = self.drive.ListFile(
+            {'q': f"'{folder_id}' in parents and trashed=false"}
+        ).GetList()
+
+        for file1 in sorted(file_list, key=lambda x: x['title']):
+            title = file1['title']
+            file_id = file1['id']
+            mime_type = file1['mimeType']
+            target_path = os.path.join(local_path, title)
+
+            if mime_type == 'application/vnd.google-apps.folder':
+                self.download_folder(file_id, target_path)
+            else:
+                print(f"Downloading file: {title} to {target_path}")
+                self.download_file(target_path, file_id)
 # Set up logging
 logging.basicConfig(level=logging.INFO)
-
+data = Drive_object()
 # Initialize Slack Bolt App
 # Make sure you have set environment variables SLACK_BOT_TOKEN and SLACK_APP_TOKEN
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 api_key = os.environ["API_KEY"]
 gpt_client = OpenAI(api_key = api_key)
+def gpt_loadfile(filepath:str):
+    file = gpt_client.files.create(
+        file=open(filepath, "rb"),
+        purpose='assistants'
+    )
+    return file.id
+
 def gpt_response(model:str, messages:str):
     completion = gpt_client.chat.completions.create(
         model=model,
-        messages=messages
+        messages=messages,
+        temperature=0.5,
+        max_tokens=1024,
     )
-    return completion.choices[0].message
+    return completion.choices[0].message.content
 def help_message(bot_user_id):
     return f"""
 <@{bot_user_id}> help
 <@{bot_user_id}> analyze-user <target_user> [--private=*true*|false]
 <@{bot_user_id}> hangout-party <group_size> <party_discription> [--private=*true*|false]
 """
-def analyze_user(target:str):
-    return f"test for {target}"
 
 
+
+def analyze_user(target:str, event_data:dict):
+    event_data['say'](f'loading data... please wait', thread_ts=event_data['thread_ts'], mrkdwn=True, ephemeral_user=event_data['user'])
+    # data.download_folder(os.environ["DATA_FOLDER_ID"], local_path='./data/')
+    event_data['say'](f'done! analyzing...', thread_ts=event_data['thread_ts'], mrkdwn=True, ephemeral_user=event_data['user'])
+    with open("./data/personas/2024_happy_hour.json", 'rb') as f:
+        content_2024 = f.read()
+    with open("./data/personas/2025_happy_hour.json", 'rb') as f:
+        content_2025 = f.read()
+    
+    response = gpt_response("gpt-4-turbo", [{"role":"user",
+                                                    "content":[
+                                                        {"type":"text",
+                                                         "text":f"""give me the information for member {target} and tell me how to talk to he/her for first time, 
+                                                         here is some data about all people
+                                                         intro of 2024 newcomer
+                                                         {content_2024}
+                                                         intro of 2025 newcomer
+                                                         {content_2025}
+                                                         """},
+                                                        ]
+                                                    }])
+
+    # shutil.rmtree('./data')
+    return f"test for {target}, response:{response}"
+
+def recommendation_user(group_size:int, party_discription:str, event_data:dict):
+    event_data['say'](f'loading data... please wait', thread_ts=event_data['thread_ts'], mrkdwn=True, ephemeral_user=event_data['user'])
+    data.download_folder(os.environ["DATA_FOLDER_ID"], local_path='./data/')
+    event_data['say'](f'done! analyzing...', thread_ts=event_data['thread_ts'], mrkdwn=True, ephemeral_user=event_data['user'])
+    
+    
+    
+    shutil.rmtree('./data')
+    return []
+    
 @app.event("app_mention")
 def handle_app_mention(event: dict, say: Say, client: slack_sdk.web.client.WebClient=None) -> None:
     bot_user_id = app.client.auth_test()["user_id"]
@@ -70,18 +183,19 @@ def handle_app_mention(event: dict, say: Say, client: slack_sdk.web.client.WebCl
     thread_ts = event.get("thread_ts") or event.get("ts")
     text_arr = text.split()
     text_arr.remove(f"<@{bot_user_id}>")
-    for idx, text in enumerate( text_arr):
-        if "=" in text:
-            key, value = text.split("=", 1)
-            text_arr[idx] = {key:value}
+    # for idx, text in enumerate( text_arr):
+    #     if "=" in text:
+    #         key, value = text.split("=", 1)
+    #         text_arr[idx] = {key:value}
     private = True
     if len(text_arr) == 0 or text_arr[0] == 'help': # show help
         reply_text = f"<@{user}> Here are the available commands:\n{help_message(bot_user_id)}"
     elif text_arr[0] == "analyze-user":
         opts, args = getopt.getopt(text_arr[1:], 'hp:', ['help', 'private='])
         help_msg = f'<@{user}> Here is the help for analyze-user:\n<@{bot_user_id}> analyze-user <target_user> [--private=*true*|false]'
+        logging.info(f'opts:{opts}\nargs:{args}')
         if args:
-            reply_text = analyze_user(args[0])
+            reply_text = analyze_user(args[0], {"say":say, "thread_ts":thread_ts, "user":user})
         else:
             reply_text = help_msg
         for opt_name, opt_value in opts:
@@ -103,7 +217,9 @@ def handle_app_mention(event: dict, say: Say, client: slack_sdk.web.client.WebCl
             else:
                 group_size = int(args[0])
                 party_description = args[1]
-                reply_text = f"<@{user}> Creating a hangout party for {group_size} people: {party_description}"
+                recondation = recommendation_user(group_size, party_description, {"say":say, "thread_ts":thread_ts, "user":user})
+                reply_text = f"<@{user}> Creating a hangout party for {group_size} people: {party_description}\nreconndation these prople:\n{' '.join(recondation)}"
+                
         else:
             reply_text = help_msg
         for opt_name, opt_value in opts:
@@ -140,106 +256,7 @@ def handle_app_mention(event: dict, say: Say, client: slack_sdk.web.client.WebCl
             say(reply_text, thread_ts=thread_ts, mrkdwn=True, ephemeral_user=user)
         else:
             say(reply_text, thread_ts=thread_ts, mrkdwn=True)
-    
-# @app.command('/analyze-user')
-# def handle_analyze_user_command(ack, body, client, logger):
-#     """
-#     Handles the /analyze-user slash command.
-#     It displays a summary file for the user. The message is private by default.
 
-#     Usage: /analyze-user <target_username> [show_everyone=true|false]
-#     """
-    
-#     ack()
-#     # logger.info(body)
-#     command_text = body.get('text', '').strip()
-#     user_id = body['user_id']
-#     channel_id = body['channel_id']
-#     parts = command_text.split()
-#     username = None
-#     show_everyone = False 
-#     params = {}
-#     for part in parts:
-#         logger.info(f'part:{part}')
-#         if '=' in part:
-#             key, value = part.split('=', 1)
-#             params[key.lower()] = value.lower()
-#         else:
-#             if username == None:
-#                 username = part
-#             else:
-#                 client.chat_postEphemeral(
-#                     channel=channel_id,
-#                     user=user_id,
-#                     text=f"Invalid value for `target_username`. you can only search only one person at same time"
-#                 )
-#     if 'show_everyone' in params:
-#         if params['show_everyone'] == 'true':
-#             show_everyone = True
-#         elif params['show_everyone'] != 'false': # Only check if not 'false'
-#             client.chat_postEphemeral(
-#                 channel=channel_id,
-#                 user=user_id,
-#                 text=f"Invalid value for `show_everyone`. Please use `true` or `false`."
-#             )
-#             return
-#     if not username:
-#         client.chat_postEphemeral(
-#             channel=channel_id,
-#             user=user_id,
-#             text="Please provide a username. Usage: /analyze-user <target_username> [show_everyone=true|false]"
-#         )
-#         return
-#     logger.info(f'username:{username}, show_everyone:{show_everyone}')
-#     try:
-#         # Construct the file path relative to the script's location
-#         # TODO: connect to google drive instead of file systeam
-#         file_path = os.path.join("./data/", username, "summary.txt")
-
-#         # Check if the summary file exists
-#         if not os.path.exists(file_path):
-#             client.chat_postEphemeral(
-#                 channel=channel_id,
-#                 user=user_id,
-#                 text=f"Sorry, I couldn't find a summary for the user '{username}' in {file_path}."
-#             )
-#             return
-
-#         # Read the content of the summary file
-#         with open(file_path, 'r') as f:
-#             summary_content = f.read()
-#         message_text = f"Here is the information for *{username}*:\n\n{summary_content}"
-#         # Send the summary as an ephemeral message
-#         if show_everyone:
-#             requester_info = f"\n_(Analysis requested by <@{user_id}>)_"
-#             client.chat_postMessage(
-#                 channel=channel_id,
-#                 text=message_text + requester_info
-#             )
-#         else: # This block runs by default
-#             client.chat_postEphemeral(
-#                 channel=channel_id,
-#                 user=user_id,
-#                 text=message_text
-#             )
-
-#     except Exception as e:
-#         logger.error(f"Error handling /analyze-user command: {e}")
-#         client.chat_postEphemeral(
-#             channel=channel_id,
-#             user=user_id,
-#             text="An error occurred while processing your request. Please try again later."
-#         )
-#     pass    
-
-# @app.command('hangout-party')
-# def handle_hangout_party_command(awk, body, client):
-#     '''
-#     Usage: /hangout-party <n_users>
-#     '''
-#     # TODO: input a discription or sth about party, analyze all user, choose n user fittest this party
-#     # TODO: open a windows to input data of party e.g. time, discription
-#     pass
 # Start the bot
 if __name__ == "__main__":
     # Use Socket Mode Handler
